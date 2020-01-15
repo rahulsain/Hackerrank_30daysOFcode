7f9c7b449bd3f17ae596c4365b06bfed1e1f9b05
RAHUL <50570677+rahulsain3000@users.noreply.github.com>
2020-01-15 20:13:39 +0530

    Create day10.cpp

---
+++ 30 days of code( c++)/day10.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+   int rem=0,s=0,t=0;
+
+
+    while(n>0)
+        {
+        rem=n%2;
+        n=n/2;
+        if(rem==1)
+         {   s++;
+           if(s>=t)
+
+            t=s;
+
+        }
+        else{
+
+            s=0;
+        }   
+    }
+cout<<t;
+return 0;
+}
