b8d966c22ad514c4e92a1d2b56062ce04b9c6e23
RAHUL <50570677+rahulsain3000@users.noreply.github.com>
2020-01-19 16:40:37 +0530

    Create day15.cpp

---
+++ 30 days of code( c++)/day15.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cstddef>
+using namespace std;	
+class Node
+{
+    public:
+        int data;
+        Node *next;
+        Node(int d){
+            data=d;
+            next=NULL;
+        }
+};
+class Solution{
+    public:
+
+      Node* insert(Node *head,int data)
+      {
+          Node** pp = &head; // pp is the pointer to the pointer head
+  while(*pp) // While head is not null (the content of pp is not null)
+     pp = &((*pp)->next); // pp receive the address of the pointer to the next node
+     *pp = new Node(data); // In this moment the content of pp is the address to the last node
+     return head;
+
+          //Complete this method
+      }
+
+      void display(Node *head)
+      {
+          Node *start=head;
+          while(start)
+          {
+              cout<<start->data<<" ";
+              start=start->next;
+          }
+      }
+};
+int main()
+{
+	Node* head=NULL;
+  	Solution mylist;
+    int T,data;
+    cin>>T;
+    while(T-->0){
+        cin>>data;
+        head=mylist.insert(head,data);
+    }	
+	mylist.display(head);
+		
+}
