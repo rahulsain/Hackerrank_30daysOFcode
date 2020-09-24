118c1136bd9bbf9d4b5e7e7fe75a47a9b6bd26f8
abraham-john-paul <abrahampaul096@gmail.com>
2020-09-24 18:14:50 +0530

    fixed errors and formatted the code

--- 30 days of code( c++)/day1.cpp
+++ 30 days of code( c++)/day1.cpp
@@ -1,29 +1,38 @@
 #include <iostream>
+#include <string>
 #include <iomanip>
-#include <limits>
 
 using namespace std;
 
-void main() {
+int main() {
     int i = 4;
     double d = 4.0;
     string s = "HackerRank";
-int j=0,sum1; double e,sum2=0.0; string t,sum3=""; 
     
     // Declare second integer, double, and String variables.
-    cin>>j>>e;
- cin.ignore(); getline(cin, t);
+    int j; 
+    double e;    
+    string t; 
+    
     // Read and save an integer, double, and String to your variables.
+    // Reading int
+    cin >> j;
+    // Reading double
+    cin >> e;
+    // Reading string
+    cin.ignore(); // ignores '\n' after entering double value
+    getline(cin, t);
     // Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
-    sum1=i+j;
 
-    sum2=d+e;
     // Print the sum of both integer variables on a new line.
-    cout<<fixed<<setprecision(1)<<sum2<<"\n";
-    sum3=s+t;
+    cout << i + j << "\n";
+
     // Print the sum of the double variables on a new line.
-    cout<<sum3;
+    cout << fixed << setprecision(1) << d + e << "\n"; // ensuring only one digit is printed after decimal point
+
     // Concatenate and print the String variables on a new line
     // The 's' variable above should be printed first.
+    cout<< s + t << "\n";
 
-    return 0;}
+    return 0;
+}
