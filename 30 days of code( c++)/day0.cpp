fc70ac67a1f167ccb042e355a325244827a96bae
abraham-john-paul <abrahampaul096@gmail.com>
2020-09-24 17:42:32 +0530

    Fixed erros: std namespace was included to the global namespace and return type of main function was changed to int from voif

--- 30 days of code( c++)/day0.cpp
+++ 30 days of code( c++)/day0.cpp
@@ -3,19 +3,19 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
-// using namespace std;
-void main() {
+using namespace std;
+int main() {
     // Declare a variable named 'input_string' to hold our input.
     string input_string; 
     
     // Read a full line of input from stdin (cin) and save it to our variable, input_string.
-    cin>>input_string;
+    cin >> input_string;
         
     // Print a string literal saying "Hello, World." to stdout using cout.
     cout << "Hello, World." << endl;
 
     // TODO: Write a line of code here that prints the contents of input_string to stdout.
-    cout<<input_string;
+    cout << input_string;
 
     return 0;
 }
