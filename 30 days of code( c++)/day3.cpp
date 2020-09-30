c2e503ff0f27eae4a59e5087596771a5a1ec8fbc
Shivang Dubey <shivangdubey8@gmail.com>
2020-10-01 04:31:49 +0530

    Day2&3 issue

--- 30 days of code( c++)/day3.cpp
+++ 30 days of code( c++)/day3.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 
 
-String main()
+int main()
 {
     int n;
     cin >> n;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
     if(n%2!=0||(n%2==0&& n>=6 && n<=20))
-    printf("weird");
-    else if((n%2==0&& n<=5 &&n>=2 )|| n>20))
-    printf("Not weird");
-    return "0";
+    printf("Weird");
+    else if((n%2==0&& n<=5 &&n>=2 )|| n>20)
+    printf("Not Weird");
+    return 0;
 }
