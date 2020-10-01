53065442ef970018b68867dcbbd3edf40515af10
Ayomal Praveen <ayomalpraveen33@gmail.com>
2020-10-01 16:30:00 +0530

    Added the first 5 days java code

---
+++ 30 days of code JAVA/day0.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class day0 {
+    public static void main(String[] args) {
+        String input_string;
+        Scanner sc=new Scanner(System.in);
+        input_string=sc.nextLine();
+        System.out.println("Hello, World.");
+        System.out.println(input_string);
+    }
+}
