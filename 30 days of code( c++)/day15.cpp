a947a01611414b29c169b61830bb6603f7433c11
Vedant Agarwal <vatheking@gmail.com>
2020-10-01 07:21:07 +0530

    Update day15.cpp

--- 30 days of code( c++)/day15.cpp
+++ 30 days of code( c++)/day15.cpp
@@ -1,41 +1,48 @@
 #include <iostream>
-#include <cstddef>
-// using namespace std;	
 
-class Solution{
-    public:
+using namespace std;
 
-      Node* insert(Node *head,int data)
-      {
-          Node** pp = &head; // pp is the pointer to the pointer head
-  while(*pp) // While head is not null (the content of pp is not null)
-     pp = &((*pp)->next); // pp receive the address of the pointer to the next node
-     *pp = new Node(data); // In this moment the content of pp is the address to the last node
-     return head;
+class Node {
+public:
+    int data;
+    Node *next;
 
-          //Complete this method
-      }
-
-      void display(Node *head)
-      {
-          Node *start=head;
-          while(start)
-          {
-              cout<<start->data<<" ";
-              start=start->next;
-          }
-      }
+    Node(int d) {
+        data = d;
+        next = NULL;
+    }
 };
-int main()
-{
-	Node* head=NULL;
-  	Solution mylist;
-    int T,data;
-    cin>>T;
-    while(T-->0){
-        cin>>data;
-        head=mylist.insert(head,data);
-    }	
-	mylist.display(head);
-		
+
+class Solution {
+public:
+    Node *insert(Node *head, int data) {
+        //Complete this method
+        if (head == NULL) head = new Node(data);
+        else {
+            Node *curr = head;
+            while (curr->next) curr = curr->next;
+            curr->next = new Node(data);
+        }
+        return head;
+    }
+
+    void display(Node *head) {
+        Node *start = head;
+        while (start) {
+            cout << start->data << " ";
+            start = start->next;
+        }
+    }
+};
+
+int main() {
+    Node *head = NULL;
+    Solution mylist;
+    int T, data;
+    cin >> T;
+    while (T-- > 0) {
+        cin >> data;
+        head = mylist.insert(head, data);
+    }
+    mylist.display(head);
 }
