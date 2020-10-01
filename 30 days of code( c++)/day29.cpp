8710ff9427f0dbc5a1219991fd2f5ab84045eee8
Raj Shah <raj.s.btechi16@ahduni.edu.in>
2020-10-01 13:40:17 +0530

    Solution to Day 29 on Hackerrank 30 Days of Code

---
+++ 30 days of code( c++)/day29.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+void max_possible_value(int n , int k)
+{
+    int temp = 0;
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = (i+1); j <= n; j++)
+        {
+          int result = i&j;
+           if((result > temp)&&(result < k))
+           {
+               temp = result;
+           }
+        }
+    }
+    std::cout<< temp<<std::endl;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string nk_temp;
+        getline(cin, nk_temp);
+
+        vector<string> nk = split_string(nk_temp);
+
+        int n = stoi(nk[0]);
+
+        int k = stoi(nk[1]);
+
+        max_possible_value(n,k);
+    }
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
