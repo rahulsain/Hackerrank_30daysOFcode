c2d19971a2250fc2424efd173db2a6eb54ef0f68
Swapnil Goyal <57146330+swap1010@users.noreply.github.com>
2020-10-01 12:49:12 +0530

    Day 5 implemented in python 3
    30 days of code Hackerrank(Day 5) implemented in python 3

---
+++ 30 days of code( python)/day5.py
@@ -0,0 +1,15 @@
+#30 days of code Hackerrank implemented in python 3
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,11):
+        print(f"{n} x {i} = {n*i}")
