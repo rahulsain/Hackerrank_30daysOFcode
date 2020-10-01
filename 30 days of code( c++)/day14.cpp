755dca756408d40c571b49f1a83814c06324a498
Vedant Agarwal <vatheking@gmail.com>
2020-10-01 07:23:51 +0530

    Update day14.cpp

--- 30 days of code( c++)/day14.cpp
+++ 30 days of code( c++)/day14.cpp
@@ -1,46 +1,52 @@
 #include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
+#include <vector>
 
-// using namespace std;
+using namespace std;
 
 class Difference {
-    
-	// Add your code here
-Difference(vector<int> a )
-{
-elements = a;
-}
-void computeDifference(){
- short int i, max = 0, min = 101;
-        for(i = 0; i < elements.size(); ++i)
-            max = elements[i] > max ? elements[i] : max;
-        for(i = 0; i < elements.size(); ++i)
-            min = elements[i] < min ? elements[i] : min;
-        maximumDifference = max - min;
-}
-}; // End of Difference class
+private:
+    vector<int> elements;
+
+public:
+    int maximumDifference;
+
+    Difference(vector<int> elements) {
+        this->elements = elements;
+    }
+
+    void computeDifference() {
+        int maximum = 0;
+
+        for (int i = 0; i < this->elements.size(); i++) {
+            for (int j = 0; j < this->elements.size(); j++) {
+                int absolute = abs(elements[i] - elements[j]);
+                if (absolute > maximum) maximum = absolute;
+            }
+        }
+
+        maximumDifference = maximum;
+    }
+};
 
 int main() {
     int N;
     cin >> N;
-    
+
     vector<int> a;
-    
+
     for (int i = 0; i < N; i++) {
         int e;
         cin >> e;
-        
+
         a.push_back(e);
     }
-    
+
     Difference d(a);
-    
+
     d.computeDifference();
-    
+
     cout << d.maximumDifference;
-    
-    return 0.0;
+
+    return 0;
 }
