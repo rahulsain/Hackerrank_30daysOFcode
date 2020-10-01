5e87ca6b77c57133eb06bfc60b32b1329038a798
gandalf0151 <gandalf0151@gmail.com>
2020-10-01 17:22:14 +0530

    Solution for day 13 and 14

---
+++ 30 days of code JAVA/day13.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+abstract class Book {
+    String title;
+    String author;
+    
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+    
+    abstract void display();
+}
+
+class MyBook extends Book {
+    int price;
+    
+    MyBook(String title, String author, int price) {
+        super(title, author);
+        this.price = price;
+    }
+    
+    void display() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+    }
+}
+
+
+
+public class Solution {
+   
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String title = scanner.nextLine();
+        String author = scanner.nextLine();
+        int price = scanner.nextInt();
+        scanner.close();
+
+        Book book = new MyBook(title, author, price);
+        book.display();
+    }
+}
---
+++ 30 days of code JAVA/day14.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+class Difference {
+  	private int[] elements;
+  	public int maximumDifference;
+    // Add your code here
+    public Difference(int[] nums) {
+        elements = nums;
+    }
+
+    public void computeDifference() {
+        Arrays.sort(elements);
+        maximumDifference = elements[elements.length - 1] - elements[0];
+    }
+
+
+
+} // End of Difference class
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Difference difference = new Difference(a);
+
+        difference.computeDifference();
+
+        System.out.print(difference.maximumDifference);
+    }
+}
