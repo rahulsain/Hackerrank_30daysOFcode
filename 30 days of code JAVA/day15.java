c3791106a0a6ab5ca23266456e5a6d21d3096e53
gandalf0151 <gandalf0151@gmail.com>
2020-10-01 17:47:14 +0530

    Solution for day 15

---
+++ 30 days of code JAVA/day15.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+class Node {
+	int data;
+	Node next;
+	Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class Solution {
+    public static  Node insert(Node head,int data)
+    {
+        if (head == null) return new Node(data);
+        Node curr = head;
+        while (curr.next != null)
+            curr = curr.next;
+        curr.next = new Node(data);
+        return head;
+    }
+
+
+	public static void display(Node head) {
+        Node start = head;
+        while(start != null) {
+            System.out.print(start.data + " ");
+            start = start.next;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Node head = null;
+        int N = sc.nextInt();
+
+        while(N-- > 0) {
+            int ele = sc.nextInt();
+            head = insert(head,ele);
+        }
+        display(head);
+        sc.close();
+    }
+}
