6b2d9b249d3d1c5a6ef0b723bd66792873e47deb
mzainuddin51 <42219739+mzainuddin51@users.noreply.github.com>
2020-10-02 11:46:08 +0530

    Solution Day 5

---
+++ Hackerrank_30daysOFcode_Go/day05.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+
+    nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+    checkError(err)
+    n := int32(nTemp)
+
+    for i := 1; i <= 10; i++{ 
+      fmt.Print(n," x ",i," = ",n*int32(i),"\n")   
+    } 
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
