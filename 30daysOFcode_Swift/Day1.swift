27da730cba4e8a00edceb044f6d1069c08b3a21d
Jayesh Vasudeva <jayesh0vasudeva@gmail.com>
2020-10-03 12:36:39 +0530

    30daysOFcode_Swift with updated README.md

---
+++ 30daysOFcode_Swift/Day1.swift
@@ -0,0 +1,25 @@
+// Day 1: Data Types
+
+var anInt:Int = 3
+// by default the decimal values in Swift are considered as Double
+var aDouble = 5.6
+var aFloat:Float = 3.4
+var str = "Hackerrank_30daysOFcode"
+
+var inInt = Int(readLine()!)
+var inDouble = Double(readLine()!)
+var inFloat = Float(readLine()!)
+var inStr = readLine()
+
+// prints the sum of the input integer with the initilaised integer
+print(anInt + inInt!)
+// prints the sum of the input double with the initilaised double
+print(aDouble + inDouble!)
+// prints the sum of the input float with the initilaised float
+print(aFloat + inFloat!)
+// prints the sum of the input string with the initilaised string
+print(str + inStr!)
+
+
+
+
