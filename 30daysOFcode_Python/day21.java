5d2c6bcfa00eee5fd2aea3657836ed832d9145d6
VenkatVuddagiri <68139968+VenkatVuddagiri@users.noreply.github.com>
2020-10-03 15:44:22 +0530

    Add day21.java
    Generics are not allowed in hackerrank in python. So, I've solved the question in java.

---
+++ 30daysOFcode_Python/day21.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Printer <T> {
+
+    /**
+    *    Method Name: printArray
+    *    Print each element of the generic array on a new line. Do not return anything.
+    *    @param A generic array
+    **/
+    
+    // Write your code here
+    public static <E> void printArray(E[] generic){
+    for(E element : generic) {
+        System.out.println(element); 
+    }
+}
+}
+
+public class Generics {
+    
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Integer[] intArray = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            intArray[i] = scanner.nextInt();
+        }
+
+        n = scanner.nextInt();
+        String[] stringArray = new String[n];
+        for (int i = 0; i < n; i++) {
+            stringArray[i] = scanner.next();
+        }
+        
+        Printer<Integer> intPrinter = new Printer<Integer>();
+        Printer<String> stringPrinter = new Printer<String>();
+        intPrinter.printArray( intArray  );
+        stringPrinter.printArray( stringArray );
+        if(Printer.class.getDeclaredMethods().length > 1){
+            System.out.println("The Printer class should only have 1 method named printArray.");
+        }
+    } 
+}
