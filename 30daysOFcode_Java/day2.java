bb503809859c2819feb39920f0927b41d0896cd0
ADITYA MISHRA <mishraaditya20142014@gmail.com>
2020-10-04 12:23:37 +0530

    Update day2.java

--- 30daysOFcode_Java/day2.java
+++ 30daysOFcode_Java/day2.java
@@ -1,18 +1,18 @@
 import java.util.*;
-public class day2 {
-    public static void main(String[] args) {
+public class Arithmetic {
+    public static void main(String[] args){
+        //creating an object "sc" to call it later
+        //this saves time
         Scanner sc=new Scanner(System.in);
-        double meal_cost=sc.nextDouble();
-        int tip_percent=sc.nextInt();
-        int tax_percent=sc.nextInt();
-        solve(meal_cost,tip_percent,tax_percent);
-    }
-    public static void solve(double meal_cost,int tip_percent,int tax_percent){
-        float tip,tax;
-        int totalCost;
-        tax=(float)meal_cost*tax_percent/100;
-        tip=(float)meal_cost*tip_percent/100;
-        totalCost=(int)(meal_cost+tip+tax);
-        System.out.println(totalCost);
+        //allocating memory space using the object "sc"
+        double mealCost = sc.nextDouble();
+        int tipPercent = sc.nextInt();
+        int taxPercent = sc.nextInt();
+        //net cost calculation done in single line saves time
+        //and helps to make the code look clear and short
+        double netCost =mealCost+(mealCost*tipPercent/100)+(mealCost*taxPercent/100);
+        //using round function to round-off the answer\
+        //then casting it into integer type
+        System.out.println((int) Math.round(netCost));
     }
 }
