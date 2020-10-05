a6a042178c380ee08d8d41595c3396d25f22cd25
Nikhil Gupta <43096630+nikkkhil067@users.noreply.github.com>
2020-10-05 11:12:47 +0530

    Update to day28 solution

--- 30daysOFcode_Python/day28.py
+++ 30daysOFcode_Python/day28.py
@@ -1,22 +1,16 @@
 #!/bin/python3
 
-import math
-import os
-import random
 import re
-import sys
 
-if __name__ == '__main__':
-    N = int(input())
+result = list()
 
-    gmail_regex = re.compile("^[a-z\.]+@gmail.com$")
-    gmail_accounts = dict()
-    for N_itr in range(N):
-        firstNameEmailID = input().split()
-        firstName = firstNameEmailID[0]
-        emailID = firstNameEmailID[1]
-        if gmail_regex.match(emailID):
-            gmail_accounts[emailID] = firstName
-    
-    sorted_names = sorted(gmail_accounts.values())
-    print(*sorted_names, sep="\n")
+for _ in range(int(input())):
+    firstNameEmailID = input().split()
+    firstName = firstNameEmailID[0]
+    emailID = firstNameEmailID[1]
+
+    if re.compile("@gmail.com$").search(emailID):
+        result.append(firstName)
+
+for name in sorted(result):
+    print(name)
