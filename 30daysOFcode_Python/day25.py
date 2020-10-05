ce98e7feba674da3b8c11111c1f58f259a1af64c
Nikhil Gupta <43096630+nikkkhil067@users.noreply.github.com>
2020-10-05 11:14:17 +0530

    Update to day25 solution

--- 30daysOFcode_Python/day25.py
+++ 30daysOFcode_Python/day25.py
@@ -1,17 +1,21 @@
 #!/bin/python3
 
 def isPrime(n):
-    if n == 1:
+    if n < 2:
         return False
-    divisor = 2
-    while divisor**2 <= n:
-        if n % divisor == 0:
+    i = 2
+    while i**2 <= n:
+        if n % i:
+            i += 1
+        else:
             return False
-        divisor += 1
     return True
 
-T = int(input())
-while T:
-    n = int(input())
-    print ("Prime" if isPrime(n) else "Not prime")
-    T -= 1
+n = int(input())
+values = [int(input()) for _ in range(n)]
+
+for v in values:
+    if isPrime(v):
+        print("Prime")
+    else:
+        print("Not prime")
