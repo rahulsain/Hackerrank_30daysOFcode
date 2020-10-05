4f917ec5d650c2edcf2c3f4a72314c0573cadadd
Rahul Sain <50570677+rahulsain3000@users.noreply.github.com>
2020-10-05 17:04:20 +0530

    Update day 26.java

--- 30daysOFcode_Java/day 26.java
+++ 30daysOFcode_Java/day 26.java
@@ -1,5 +1,3 @@
-
-
 import java.util.Scanner;
 
 public class Solution {
@@ -8,18 +6,18 @@ public class Solution {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter Actual day");
+        //System.out.println("Please enter Actual day");
         int actualDay = sc.nextInt();
-        System.out.println("Please enter Actual Month");
+        //System.out.println("Please enter Actual Month");
         int actualMonth = sc.nextInt();
-        System.out.println("Please enter Actual Year");
+        //System.out.println("Please enter Actual Year");
         int actualYear = sc.nextInt();
 
-        System.out.println("Please enter Expected Day");
+        //System.out.println("Please enter Expected Day");
         int expectedDay = sc.nextInt();
-        System.out.println("Please enter Expected Month");
+        //System.out.println("Please enter Expected Month");
         int expectedMonth = sc.nextInt();
-        System.out.println("Please enter Expected Year");
+        //System.out.println("Please enter Expected Year");
         int expectedYear = sc.nextInt();
 
 
