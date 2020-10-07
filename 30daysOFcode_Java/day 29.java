6805cc1f627565b2a7bec0f56e8ac5ecd83837ff
ADITYA MISHRA <mishraaditya20142014@gmail.com>
2020-10-07 16:42:16 +0530

    Update day 29.java

--- 30daysOFcode_Java/day 29.java
+++ 30daysOFcode_Java/day 29.java
@@ -1,24 +1,21 @@
-
 import java.util.*;
-
-public class day29 {
-
+//most precised and simple code for this problem...
+//simplicity of the code makes it extremely easy to understand...
+public class Solution {
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner  sc=new Scanner(System.in);
-        int t=sc.nextInt();
-        while(t-->0){
-            int n=sc.nextInt();
-            int k=sc.nextInt();
-            int max=0;
-            for(int i=1;i<n;i++){
-                for(int j=i+1;j<=n;j++){
-                    int res= i&j;
-                    if(res>max && res<k)
-                        max=res;
-                }
-            }
+        //object crreation  for memory allocation to variables...
+        Scanner sc=new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i=1;i<=t;i++){
+        int n,k,max=0;
+        n=sc.nextInt();
+        k=sc.nextInt();
+            for(int j=1;j<n;j++)
+            {
+                for(int l=j+1;l<=n;l++){
+                    //bitwise-and operation...
+                    int temp=l&j;
+                    if((temp<k) && (temp>max)) max=temp;
+                }}
             System.out.println(max);
-        }
-    }
-}
+}}}
