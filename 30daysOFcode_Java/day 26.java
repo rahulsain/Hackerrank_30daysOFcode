2384ea38d7be94e4274cbe5e1d721c0d070c3f7b
ADITYA MISHRA <mishraaditya20142014@gmail.com>
2020-10-07 16:14:26 +0530

    Update day 26.java

--- 30daysOFcode_Java/day 26.java
+++ 30daysOFcode_Java/day 26.java
@@ -1,36 +1,49 @@
+
+
 import java.util.Scanner;
-import java.time.LocalDate;
+
 public class Solution {
-    //LocalDate Function Makes it Extremely easy to manage dates...
-    private static LocalDate readDate(Scanner sc) {
-        int day   = sc.nextInt();
-        int month = sc.nextInt();
-        int year  = sc.nextInt();
-        return LocalDate.of(year, month, day);
-    }
+
     public static void main(String[] args) {
-        //object is called from readDate,
-        //then object is read from LocalDate,
-        //Finally the value is stored in the variables...
-        Scanner sc=new Scanner(System.in);
-        LocalDate returnDate=readDate(sc);
-        LocalDate expectDate=readDate(sc);
-        
-        int fine;
-        //CASE : 1
-        if (returnDate.isEqual(expectDate)||returnDate.isBefore(expectDate))fine=0;
-        //CASE : 2
-        else if(returnDate.getMonth()==expectDate.getMonth() && 
-                returnDate.getYear()==expectDate.getYear()) 
-        {
-            fine=15*(returnDate.getDayOfMonth()-expectDate.getDayOfMonth());
-        }
-        //CASE : 3
-        else if(returnDate.getYear() == expectDate.getYear()) 
-            fine=500*(returnDate.getMonthValue()-expectDate.getMonthValue());
-        //CASE : 4
-        else fine=10000;
-        //OUTPUT...
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        Scanner sc = new Scanner(System.in);
+
+        int actualDay = sc.nextInt();
+        int actualMonth = sc.nextInt();
+        int actualYear = sc.nextInt();
+
+        int expectedDay = sc.nextInt();
+        int expectedMonth = sc.nextInt();
+        int expectedYear = sc.nextInt();
+
+
+        int fine = computeFine(actualDay, actualMonth, actualYear, expectedDay, expectedMonth, expectedYear);
+
         System.out.println(fine);
     }
+
+    static int computeFine(int actualDay, int actualMonth, int actualYear, int expectedDay, int expectedMonth, int expectedYear) {
+
+        int fine = 0;
+
+        if (actualYear > expectedYear) {
+            fine = 10000;
+        }
+        else if (actualYear == expectedYear) {
+
+            if (actualMonth > expectedMonth) {
+                fine = (actualMonth - expectedMonth) * 500;
+            }
+            else if (actualMonth == expectedMonth) {
+
+                if (actualDay > expectedDay) {
+                    fine = (actualDay - expectedDay) * 15;
+                }
+            }
+        }
+
+        return fine;
+    }
 }
+
