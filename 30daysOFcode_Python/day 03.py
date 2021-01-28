013d559a641cdea1a60b9e1ce95816890144bae9
Utkarsh Mishra <76392681+Utkarsh1504@users.noreply.github.com>
2021-01-28 11:22:13 +0530

    Update day 03.py

--- 30daysOFcode_Python/day 03.py
+++ 30daysOFcode_Python/day 03.py
@@ -1,25 +1,9 @@
 '''
 #!/bin/python
 # compatible with python3
-import sys
 
-
-n = int(input().strip())
-
-# if 'n' is NOT evenly divisible by 2 (i.e.: n is odd)
-if n%2==1:
-    ans = "Weird"
-
-elif n>20:
-    ans = "Not Weird"
-
-elif n>=6:
-    ans = "Weird"
-
+n = int(input())
+if(n%2==1) or n in range (5,21):
+    print("Weird")
 else:
-    ans = "Not Weird"
-
-print(ans)
-'''
-
-(lambda n : print( "Weird" if  n%2 else "Not Weird" if 2<=n<=5 else "Weird" if 6<=n<=20 else "Not Weird"     ) ) ( int(input().strip()))
+    print("Not Weird")
