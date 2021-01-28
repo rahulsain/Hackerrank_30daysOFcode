72d88bdd93d78d0c92298b0d6e2dabb6da013e01
Utkarsh Mishra <76392681+Utkarsh1504@users.noreply.github.com>
2021-01-28 11:20:36 +0530

    Update day 03.cpp

--- 30daysOFcode_C++/day 03.cpp
+++ 30daysOFcode_C++/day 03.cpp
@@ -1,20 +1,16 @@
 #include<iostream>
 using namespace std;
 
-int main() {
-    int n;
-    cin >> n;
-    if(n%2 != 0) {
-    	cout << "Weird";	
-	}
-	else if(n%2 ==0 && (n>=6 && n<=20)){
-		cout << "Weird";
-	}
-    else if(n>20 && n%2==0){
-    	cout << "Not Weird";	
-	}
-	else if(n%2==0 && n<=5 && n>=2 ){
-		cout << "Not Weird";
-	}
-    return 0;
+int main()
+{
+    int N;
+    cin >> N;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    if( N%2 == 1 || (N > 5 && N < 21)){
+        cout<<"Weird"<<endl;
+    }
+   else{
+       cout<<"Not Weird"<<endl;
+   }
+	return 0;
 }
